33238b644958bbcf6f83869bab81be15eefefeeb
David Ladowitz <david@ladowitz.com>
2016-07-26 13:46:29 -0700

    adding last two challenges

---
+++ deployable.md
@@ -0,0 +1,12 @@
+## i. What does the term "deployability" mean to you?
+
+1. Code Reviews or Pair Programming
+2. Test coverage
+2. Staging Environment
+3. Continuous Integration or Continuous Development System
+
+Optional: Feature Flags to turn on or off features on the fly.
+
+## ii. As a developer, what can you do to make your RoR applications "highly deployable”?
+
+The most important aspect of deployability is good test coverage. Unit tests on models, controllers, routes and helpers (Rspec, MiniTest). Additionally integration tests of end to end functionality along with the UI (Cucumner, Capybaray, Selenium, Mocha).
---
+++ log_search.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Challenge A: find all .log files within /var/logs folder (including within nested subfolders etc)
+find /var/log -name "*log" -type f -exec ls -l {} \;
+
+# Challenge B: output contents of every log file, using one shell liner
+for f in $( find /var/log -name "*.log" -type f -exec ls -l {} \; ); do; tail -n +1 -- $f > ~/Downloads/log-data.txt; done
+
+# Challenge C: output lines containing timestamps of form 'YYYY-MM-DD HH:MM:SS'
+for f in $( find /var/log -name "*.log" -type f -exec ls -l {} \; );
+do;
+  tail -n +1 -- $f | xargs -L 1 bash  -c 'printf "[%s] %s\n" "$(date +%Y-%m-%d\ %H:%M:%S )" "$*" ' bash > ~/Downloads/timestamp-prepend.txt
+done
