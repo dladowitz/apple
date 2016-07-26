cccd670427d90522f1652fedfbaef36f865c163d
David Ladowitz <david@ladowitz.com>
2016-07-26 13:53:55 -0700

    fixing log search typos

--- log_search.sh
+++ log_search.sh
@@ -4,10 +4,10 @@
 find /var/log -name "*log" -type f -exec ls -l {} \;
 
 # Challenge B: output contents of every log file, using one shell liner
-for f in $( find /var/log -name "*.log" -type f -exec ls -l {} \; ); do; tail -n +1 -- $f > ~/Downloads/log-data.txt; done
+for f in $( find /var/log -name "*.log" -type f -exec ls -l {} \; ); do tail -n +1 -- $f; done
 
 # Challenge C: output lines containing timestamps of form 'YYYY-MM-DD HH:MM:SS'
 for f in $( find /var/log -name "*.log" -type f -exec ls -l {} \; );
-do;
-  tail -n +1 -- $f | xargs -L 1 bash  -c 'printf "[%s] %s\n" "$(date +%Y-%m-%d\ %H:%M:%S )" "$*" ' bash > ~/Downloads/timestamp-prepend.txt
+do
+  tail -n +1 -- $f | xargs -L 1 bash  -c 'printf "[%s] %s\n" "$(date +%Y-%m-%d\ %H:%M:%S )" "$*" ' bash
 done
